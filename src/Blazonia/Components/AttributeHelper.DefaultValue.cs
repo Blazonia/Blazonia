8d425fa0078706c867369ce06fe93d2e8fd6742a
CeSun <ce.sun@outlook.com>
2025-12-27 21:38:31 +0800

    update sln & proj conf fix warning update github action

--- src/Blazonia/Components/AttributeHelper.DefaultValue.cs
+++ src/Blazonia/Components/AttributeHelper.DefaultValue.cs
@@ -5,7 +5,7 @@ namespace Blazonia.Components;
 
 public static partial class AttributeHelper
 {
-    public static object? GetDefaultValue<TValue>(this DirectPropertyBase<TValue> directProperty, Type ownerType)
+    public static object GetDefaultValue<TValue>(this DirectPropertyBase<TValue> directProperty, Type ownerType)
     {
         var unset = directProperty.GetUnsetValue(ownerType);
         return unset;
