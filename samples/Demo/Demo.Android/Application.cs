1f18671711311598b3d26bc4c4420adba9e2f2ca
CeSun <ce.sun@outlook.com>
2026-02-27 11:08:44 +0800

    update to .net10

---
+++ samples/Demo/Demo.Android/Application.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+using Avalonia.Android;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Android;
+
+[Application]
+public class Application : AvaloniaAndroidApplication<App>
+{
+    protected Application(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+    {
+    }
+}
